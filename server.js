1d4f1bc87e68050177361875359f777a9c0768a8
Umair Ali <umairmamoor@gmail.com>
2021-08-31 22:30:13 +0500

    different routes and functions

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.get('/api/animals', (req, res) => {
    res.json(results);
   });
 
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 app.listen(PORT, () => {
     console.log(`API server on port 300!`);
 })
